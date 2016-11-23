8fe1e21c562adb461ca4425b5e62dc26f79b50ad
GarethLivermore <livermoregs@cardiff.ac.uk>
2016-11-23 22:31:51 +0000

    Create test9.sql
    A report that shows the total number of orders by Customer since December 31, 1996.

---
+++ test9.sql
@@ -0,0 +1,7 @@
+SELECT c.CompanyName, COUNT(o.OrderID) AS NumOrders
+FROM Customers c JOIN Orders o ON
+	(c.CustomerID = o.CustomerID)
+WHERE OrderDate > '1996-12-31'
+GROUP BY c.CompanyName
+HAVING COUNT(o.OrderID) > 15
+ORDER BY NumOrders DESC;
