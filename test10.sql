81458c9833093024a9d6768e857df05aa3dacf07
GarethLivermore <livermoregs@cardiff.ac.uk>
2016-11-23 22:32:51 +0000

    Create test10.sql
    Report showing the number of employees working in cities.

---
+++ test10.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(DISTINCT e.EmployeeID) AS numEmployees,
+	COUNT(DISTINCT c.CustomerID) AS numCompanies,
+	e.City, c.City
+FROM Employees e JOIN Customers c ON
+	(e.City = c.City)
+GROUP BY e.City, c.City
+ORDER BY numEmployees DESC;
